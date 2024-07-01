2f12838deb9df32d11fccfe484c9c2b4e44dd5bc
Siva Kumar <sivakumar9399959803@gmail.com>
2024-07-01 08:39:31 +0530

    tf-module-eks

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
-resource "aws_eks_cluster" "example" {
-  name     = "${var.component}-${var.env}-eks"
+### Eks cluster
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.env}-eks"
   role_arn = aws_iam_role.eks-role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
+  }
+}
+
+### Node group
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.env}-ng"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = var.subnet_ids
+  capacity_type   = var.capacity_type
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.min_size
+    max_size     = var.max_size
+    min_size     = var.min_size
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
-variable "component" {}
 variable "env" {}
 variable "subnet_ids" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "capacity_type" {}
+variable "instance_types" {}
