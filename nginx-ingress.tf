d4028c7d4336849cf985f23af42de9da311e6153
Siva Kumar <sivakumar9399959803@gmail.com>
2024-07-16 16:29:33 +0530

    tf-module-eks

---
+++ nginx-ingress.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "null" {
+
+  triggers = {
+    always = timestamp()
+  }
+
+  depends_on = [
+    aws_eks_cluster.cluster
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+aws eks update-kubeconfig --name ${var.env}-eks
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress ingress-nginx/ingress-nginx -f ${path.module}/nginx-values.yaml
+EOF
+  }
+}
