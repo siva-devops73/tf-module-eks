01ec9d9dba9269d3dfe02bcc2d0fe896181b451e
Siva Kumar <sivakumar9399959803@gmail.com>
2024-07-20 10:45:16 +0530

    tf-module-eks

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 ### Eks cluster
-resource "aws_eks_cluster" "eks" {
+resource "aws_eks_cluster" "cluster" {
   name     = "${var.env}-eks"
   role_arn = aws_iam_role.eks-role.arn
 
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "eks" {
 
 ### Node group
 resource "aws_eks_node_group" "nodes" {
-  cluster_name    = aws_eks_cluster.eks.name
+  cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.env}-ng"
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_ids
--- nginx-ingress.tf
+++ nginx-ingress.tf
@@ -5,14 +5,14 @@ resource "null_resource" "null" {
   }
 
   depends_on = [
-    aws_eks_cluster.eks
+    aws_eks_cluster.cluster
   ]
   provisioner "local-exec" {
     command = <<EOF
 aws eks update-kubeconfig --name ${var.env}-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm upgrade ingress ingress-nginx/ingress-nginx -f ${path.module}/nginx-values.yaml
+helm upgrade -i ingress ingress-nginx/ingress-nginx -f ${path.module}/nginx-values.yaml
 EOF
   }
 }
